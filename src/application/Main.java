cc64776a9d99288faefc318e6672f89bf9ca6930
Yuri Alves <yuri.o.alves2@outlook.com>
2020-02-21 17:06:12 -0300

    Class Piece e Board Criadas

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,13 @@
 package Application;
 
-import javax.crypto.spec.PSource;
-
-import BoardGame.Position;
+import BoardGame.Board;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 4);
-
-		System.out.println(pos);
+		Board board = new Board(8, 8);
+		
 	}
 
 }
